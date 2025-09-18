fee12d11497a29b93d7f60c6dc3d3d40c79cf434
christdevtech <michael@christdev.com>
2025-09-18 11:41:30 +0100

    fix(next.config): add additional allowed image host for remote patterns
    Add 'https://witelectrical.apps.christdev.com' to the list of allowed remote image hosts to enable image loading from this domain

--- next.config.js
+++ next.config.js
@@ -6,14 +6,16 @@ import redirects from './redirects.js'
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[process.env.NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
+      ...[process.env.NEXT_PUBLIC_SERVER_URL, 'https://witelectrical.apps.christdev.com'].map(
+        (item) => {
+          const url = new URL(item)
 
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
+          return {
+            hostname: url.hostname,
+            protocol: url.protocol.replace(':', ''),
+          }
+        },
+      ),
     ],
   },
   webpack: (webpackConfig) => {
