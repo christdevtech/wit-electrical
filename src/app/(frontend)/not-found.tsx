ead60087c293fc093eb9e969e9ccef9dc5ed6dee
christdevtech <michael@christdev.com>
2026-01-18 00:18:10 +0100

    feat: Implement a custom 404 Not Found page.

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 
 import { Button } from '@/components/ui/button'
+import PageClient from './[slug]/page.client'
 
 export default function NotFound() {
   return (
     <div className="container py-28">
+      <PageClient />
       <div className="prose max-w-none">
         <h1 style={{ marginBottom: 0 }}>404</h1>
         <p className="mb-4">This page could not be found.</p>
